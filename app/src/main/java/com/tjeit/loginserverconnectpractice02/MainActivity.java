20586eff9ca59233c5b66a39bb15c90932d221d7
yhkim <kvoyant@gmail.com>
2019-05-15 16:47:46 +0900

    데이터바인딩/베이스액티비티

--- app/src/main/java/com/tjeit/loginserverconnectpractice02/MainActivity.java
+++ app/src/main/java/com/tjeit/loginserverconnectpractice02/MainActivity.java
@@ -1,13 +1,34 @@
 package com.tjeit.loginserverconnectpractice02;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tjeit.loginserverconnectpractice02.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
