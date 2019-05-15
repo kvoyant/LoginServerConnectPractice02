22b1357fd4d9c79c89966bf60df78d0f12930534
yhkim <kvoyant@gmail.com>
2019-05-16 02:17:33 +0900

    로그인결과 로그켓에 출력하기 - BASE_URL 추가

--- app/src/main/java/com/tjeit/loginserverconnectpractice02/utils/ConnectServer.java
+++ app/src/main/java/com/tjeit/loginserverconnectpractice02/utils/ConnectServer.java
@@ -19,7 +19,7 @@ import okhttp3.Response;
 public class ConnectServer {
 
     //    서버의 근본 주소
-    private final static String BASE_URL = "";
+    private final static String BASE_URL = "http://delivery-dev-389146667.ap-northeast-2.elb.amazonaws.com";
 
     //    서버의 응답을 처리하는 역할을 담당하는 인터페이스
     public interface JsonResponseHandler {
