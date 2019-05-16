ad99a0995a36d7b5b995eb7f0b2193a04a275fc6
yhkim <kvoyant@gmail.com>
2019-05-16 18:31:30 +0900

    로그인 실패시 AlertDialog로 경고

--- app/src/main/java/com/tjeit/loginserverconnectpractice02/LoginActivity.java
+++ app/src/main/java/com/tjeit/loginserverconnectpractice02/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.tjeit.loginserverconnectpractice02;
 
+import android.app.AlertDialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import com.tjeit.loginserverconnectpractice02.databinding.ActivityLoginBinding;
 import com.tjeit.loginserverconnectpractice02.utils.ConnectServer;
 import com.tjeit.loginserverconnectpractice02.utils.ContextUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity {
@@ -35,13 +37,36 @@ public class LoginActivity extends BaseActivity {
 
 //                2.받아온 아이디와 비번이 정말로 올바른 회원인지? 검사
 //                  아이디/비번이 모두 동일한 사람이 회원 명부에 있는지? 검사.
-//                test123 / test123
+//                test123 / Test!123
 //                iu0001 / Test!123
 //                testorder1 / testorder1
 //                gggg1111 / gggg1111
                 ConnectServer.postRequestSignIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
                     @Override
                     public void onResponse(JSONObject json) {
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    int code = json.getInt("code");
+
+                                    if(code == 200) {
+//                                        로그인 성공 !
+                                    }
+                                    else {
+//                                        로그인 실패. AlertDialog
+                                        AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                                        alert.setTitle("로그인 실패 알림");
+                                        alert.setMessage(json.getString("message"));
+                                        alert.setPositiveButton("확인", null);
+                                        alert.show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
 
                     }
                 });
